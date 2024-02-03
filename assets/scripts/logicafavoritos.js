8e3600a5b3c4d6630140d0f7a2f25f89b6ab10b1
dsiney2021664 <dsiney-2021664@kinal.edu.gt>
2024-02-03 15:01:07 -0600

    Se agrego favoritos.

---
+++ assets/scripts/logicafavoritos.js
@@ -0,0 +1,8 @@
+function redirigirADetalles(nombre) {
+    var nombreCodificado = encodeURIComponent(nombre);
+
+    window.location.href = 'detallesContacto.html?nombre=' + nombreCodificado;
+}
+
+
+
