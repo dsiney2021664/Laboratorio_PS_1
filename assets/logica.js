8b9b278cd73bf2c16e894c6ebc862c5734ddbdd5
dsiney2021664 <dsiney-2021664@kinal.edu.gt>
2024-02-03 03:08:08 -0600

    Se agrego un contenedor con 5 contactos, fue básico ya que todo ira en los detalles de contacto y se agrego un boton nuevo que a los campos minimos para crear un contacto.

--- assets/logica.js
+++
@@ -1,6 +0,0 @@
-function validarFormulario(){
-    var correo = document.getElementById('correo').value;
-    var contrasena = document.getElementById('contrasena').value;
-
-    return (correo !== '' && contrasena !== '') ? true : (alert('Complete todos los campos.'), false);
-}
